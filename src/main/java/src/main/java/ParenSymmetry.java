e40367cbd3a3a6b80978b97f3f70f76e9c6e2ed0
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-03-05 15:25:05 -0500

    Finished

--- src/main/java/src/main/java/ParenSymmetry.java
+++ src/main/java/src/main/java/ParenSymmetry.java
@@ -1,18 +1,54 @@
 package src.main.java;
+import java.lang.String;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class ParenSymmetry {
 
 
     private Boolean isBalanced(String s) {
         // implement this method
-        return null;
+        int openNumber = 0;
+        int closeNumber = 0;
+        for(int i =0; i<=s.length()-1;i++){
+            if(s.charAt(i)=='('){
+                openNumber++;
+            }
+            if(s.charAt(i)==')'){
+                closeNumber++;
+            }
+        }
+        if(openNumber == closeNumber){
+            return true;
+        }
+        else
+            return false;
+
     }
 
     private void checkFile(String filename) {
         // open file named filename
+        try{
+        File myObj = new File("/Users/diksha/Desktop/Projects/ParenSymmetry/TestStrings1.txt");
 
         // for each line in the file
+        Scanner myReader = new Scanner (myObj);
             // read the line
+        while (myReader.hasNext()){
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+        myReader.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println("An error occured");
+            e.printStackTrace();
+        }
+
+
             // print whether or not the line's parenthesis are balanced
 
         // CLOSE the file
